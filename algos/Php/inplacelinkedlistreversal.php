4f51af61bd5ab61386a46c833784e1a49e9664f4
Srinivasan <ssingan@yahoo-inc.com>
2015-06-06 19:34:05 -0700

    Product of integers and linkedlist attempt

--- algos/Php/inplacelinkedlistreversal.php
+++ algos/Php/inplacelinkedlistreversal.php
@@ -5,3 +5,98 @@
  * Date: 6/4/15
  * Time: 2:08 PM
  */
+
+
+    class LinkedList {
+
+
+        private  $data = 0;
+
+        private  $next = null;
+
+
+        function  __construct($data, $next)
+        {
+
+            $this->data = $data;
+
+            $this->next = $next;
+        }
+
+
+
+        function getNext() {
+            return $this->next;
+        }
+
+        function setNext($next) {
+            $this->next=$next;
+        }
+
+
+        function getData() {
+            return $this->data;
+        }
+
+    }
+
+
+    function reverseList(LinkedList $head) {
+
+
+
+        if ($head == null || $head->getNext() == null) {
+            return $head;
+        }
+
+
+        $next  = new LinkedList($head->getData(), $head->getNext());
+
+        $head->setNext(null);
+
+        if ($next->getNext() == null) {
+            $next->setNext($head);
+            $head = $next;
+            return $head;
+        }
+
+
+        while (true) {
+
+            $next = $head->getNext();
+
+            $head = $next;
+
+
+
+
+        }
+        return $head;
+
+    }
+
+
+    testReverseList();
+
+
+    function testReverseList() {
+
+        $head = new LinkedList(1, new LinkedList(2, new LinkedList(3, new LinkedList(4, null))));
+
+        $reverse = new LinkedList(4, new LinkedList(3, new LinkedList(2, new LinkedList(1, null))));
+
+        var_dump($head);
+
+        var_dump($reverse);
+
+
+        if ($reverse == reverseList($head)) {
+            echo "Reversal works";
+        } else {
+            echo "Reversal failed";
+        }
+
+
+    }
+
+
--- algos/Php/productofintegers.php
+++ algos/Php/productofintegers.php
@@ -5,3 +5,64 @@
  * Date: 6/3/15
  * Time: 9:29 PM
  */
+
+test_product_of_arrays_without_division();
+
+function product_of_arrays_without_division($input_array) {
+
+
+    if (empty($input_array)) {
+        return $input_array;
+    }
+
+    $successive_products = array();
+
+    $successive_products[0]=1;
+
+    for( $i=1; $i<= count($input_array)-1; $i++) {
+        $successive_products[$i] = $successive_products[$i-1] * $input_array[$i-1];
+    }
+
+    $previous_products = array();
+
+    $previous_products[count($input_array)-1] = 1;
+
+    for($i=count($input_array)-2; $i>=0 ; $i--) {
+        $previous_products[$i] = $previous_products[$i+1] * $input_array[$i+1];
+    }
+
+    $final_product = array();
+
+    for($i=0;$i<count($input_array);$i++) {
+        $final_product[$i] = $successive_products[$i] * $previous_products[$i];
+    }
+
+    return $final_product;
+
+}
+
+
+function test_product_of_arrays_without_division() {
+    if (product_of_arrays_without_division(array()) === array()) {
+        echo "empty array works\n";
+    } else {
+        echo "empty array does not work\n";
+    }
+
+
+    $result = product_of_arrays_without_division(array(1,2,3));
+    if ($result === array(6,3,2)) {
+        echo "1,2,3 array works\n";
+    } else {
+        echo "1,2,3 array does not work\n";
+    }
+
+    $result = product_of_arrays_without_division(array(0,2,3,3,2));
+    if ($result === array(36,0,0,0,0)) {
+        echo "0,2,3,3,2 array works\n";
+    } else {
+        echo "0,2,3,3,2 array does not work\n";
+    }
+
+}
+
